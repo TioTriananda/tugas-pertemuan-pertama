51d002dbef1d6fffe323401c97d9e42126197915
Tio Triananda <101992861+TioTriananda@users.noreply.github.com>
2022-09-01 09:24:32 +0700

    tugas pertemuan pertama

---
+++ lib/kalkulator.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  int a = 8;
+  int b = 4;
+  int c = a + b;
+  int d = a - b;
+  int e = a * b;
+  double f = a / b;
+
+
+  //penjumlahan
+  print('$a + $b hasil penjumlahan = $c');
+
+  //pengurangan
+  print('$a - $b hasil pengurangan = $d');
+
+  //perkalian
+  print('$a * $b hasil perkalian = $e');
+
+  //pembagian
+  print('$a / $b hasil pembagian = $f');
+}
